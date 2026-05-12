8c17fc3fcd187fd9477a5058be4a34b6bd5f4c24
geisserml <geisserml@gmail.com>
2026-05-12 21:22:27 +0200

    Fix get_version
    missing `|` in run block!

---
+++ utils/get_version.py
@@ -0,0 +1,14 @@
+# SPDX-FileCopyrightText: 2026 geisserml <geisserml@gmail.com>
+# SPDX-License-Identifier: BSD-3-Clause
+
+import re
+from pathlib import Path
+
+PROJECT_DIR = Path(__file__).parents[1].resolve()
+pyproject_file = PROJECT_DIR/"pyproject.toml"
+
+content = pyproject_file.read_text()
+match = re.search(r'version = "([\d.]+)"', content)
+assert match
+version = match.group(1)
+print(version, end="")
