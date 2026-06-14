2d676f2457d09ebaf18a82eaecd080498f347643
geisserml <geisserml@gmail.com>
2026-06-14 18:05:28 +0200

    Proper compiler toolset abstraction
    Setting CXX without setting the right AR as well turned out to be
    problematic on some setups.

--- build_gn.py
+++ build_gn.py
@@ -5,13 +5,16 @@ import os
 import sys
 import stat
 import shutil
+import argparse
 import subprocess
 from pathlib import Path
+from collections import namedtuple
 
 PROJECT_DIR = Path(__file__).parent.resolve()
 SBUILD_DIR = PROJECT_DIR/"sbuild"
 GN_DIR = SBUILD_DIR/"gn"
 GN_REV = "3357c4f51b1a9e676378c695dd9c7e9911c35ee6"
+
 
 def log(*args, **kwargs):
     print(*args, **kwargs, file=sys.stderr)
@@ -20,24 +23,90 @@ def run_cmd(command, cwd, **kwargs):
     log(f"{command} (cwd={cwd})")
     return subprocess.run(command, cwd=cwd, check=True, **kwargs)
 
-def make_executable(path):
+def _make_executable(path):
     path.chmod(path.stat().st_mode | stat.S_IXUSR | stat.S_IXGRP | stat.S_IXOTH)
 
-def build_gn():
+def _env_prepend(key, value, sep):
+    tail = os.environ.get(key, "")
+    if tail:
+        tail = sep + tail
+    os.environ[key] = value + tail
+
+
+CompilerToolset = namedtuple("CompilerToolset", ("CXX", "AR", "LD"), defaults=(None, ))
+
+def build(toolset):
+    
     # GN's build system runs git commands that assume a full checkout
     # --depth 1 checkout would need a manually created sbuild/gn/out/last_commit_position.h and passing "--no-last-commit-position" to build/gen.py - probably not worth the trouble for now.
     SBUILD_DIR.mkdir(exist_ok=True)
     if not GN_DIR.exists():
         run_cmd(["git", "clone", "https://gn.googlesource.com/gn/"], cwd=SBUILD_DIR)
         run_cmd(["git", "checkout", GN_REV], cwd=GN_DIR)
-    env = os.environ.copy()
-    env["CXX"] = os.environ.get("CXX", "g++")
-    run_cmd([sys.executable, "build/gen.py", "--no-static-libstdc++", "--allow-warnings"], cwd=GN_DIR, env=env)
+    
+    env = os.environ
+    if toolset:
+        env = env.copy()
+        env["CXX"] = toolset.CXX
+        env["AR"] = toolset.AR
+        env["LD"] = toolset.LD or toolset.CXX
+    
+    args = [sys.executable, "build/gen.py", "--allow-warnings", "--no-static-libstdc++"]
+    run_cmd(args, cwd=GN_DIR, env=env)
     # TODO on CI, build and run unittests
     run_cmd(["ninja", "-C", "out", "gn"], cwd=GN_DIR)
     target_path = PROJECT_DIR/"src"/"gn_dist"/"gn"
     shutil.copyfile(GN_DIR/"out"/"gn", target_path)
-    make_executable(target_path)
+    _make_executable(target_path)
+
+
+def assisted_build(compiler=None, toolprefix="", clang_path=None):
+    
+    if compiler is None:
+        compiler = "gcc"
+    
+    if compiler == "gcc":
+        toolset = CompilerToolset(CXX=toolprefix+"g++", AR=toolprefix+"ar")
+    elif compiler == "clang":
+        toolset = CompilerToolset(CXX="clang++", AR="llvm-ar")
+        if clang_path:
+            _env_prepend("PATH", str(clang_path/"bin"), os.pathsep)
+    elif compiler == "custom":
+        toolset = CompilerToolset(
+            CXX=os.environ["CXX"], AR=os.environ["AR"], LD=os.environ.get("LD"),
+        )
+    else:
+        assert False, compiler
+    
+    build(toolset)
+
+
+def parse_args(argv):
+    parser = argparse.ArgumentParser(
+        description="GN build script",
+    )
+    parser.add_argument(
+        "-c", "--compiler",
+        choices=("gcc", "clang", "custom"),
+        help = "The compiler to use.",
+    )
+    parser.add_argument(
+        "--toolprefix",
+        default = "",
+        help = "(gcc only) Toolprefix to use.",
+    )
+    parser.add_argument(
+        "--clang-path",
+        type = lambda p: Path(p).expanduser().resolve(),
+        help = "(clang only) Clang root dir to use.",
+    )
+    return parser.parse_args(argv)
+
+
+def main():
+    args = parse_args(sys.argv[1:])
+    assisted_build(**vars(args))
+
 
 if __name__ == "__main__":
-    build_gn()
+    main()
--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
 # SPDX-FileCopyrightText: 2026 geisserml <geisserml@gmail.com>
 # SPDX-License-Identifier: BSD-3-Clause
 
+import os
 import sys
+import shlex
 from pathlib import Path
 import setuptools
 from setuptools.command.build_py import build_py
@@ -11,12 +13,14 @@ except ImportError:
     from wheel.bdist_wheel import bdist_wheel
 
 sys.path.insert(0, str(Path(__file__).parent))
-from build_gn import build_gn  # local
+from build_gn import parse_args, assisted_build  # local
 
 
 class BuildPyClass(build_py):
     def run(self):
-        build_gn()
+        build_params = os.environ.get("BUILD_PARAMS", "")
+        args = parse_args(shlex.split(build_params))
+        assisted_build(**vars(args))
         build_py.run(self)
 
 class BdistWheelClass(bdist_wheel):
